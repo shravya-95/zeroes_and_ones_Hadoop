dc8503698600a59bdaee05a902638fcbe60b708b
Mourya Karan Reddy <bmouryakaranreddy@gmail.com>
2020-04-09 05:45:57 -0400

    changed logic

--- sql/ddl_create_product_region_sales_partitioned.sql
+++ sql/ddl_create_product_region_sales_partitioned.sql
@@ -6,17 +6,17 @@
 SET VAR:database_name=zeroes_and_ones_sales;
 
 --Create Parquet Product Region Sales Partitioned Table
+
 CREATE TABLE IF NOT EXISTS ${var:database_name}.product_region_sales_partition
 PARTITIONED BY (region,sales_year,sales_month)
+COMMENT 'Parquet Product, Region and Sales partitioned table'
 STORED AS Parquet
 AS
-Select se.order_id,se.sales_person_id,se.customer_id,se.product_id,p.name as product_name,cast(p.price as decimal(15,2)) as product_price,
-se.quantity,cast(se.quantity*p.price as decimal(15,2)) as total_sales_amount, se.sale_date as order_date,se.region,date_part('year',se.sale_date) as sales_year,
-decode(date_part('month',se.sale_date), 1, "Jan", 2, "Feb", 3, "Mar",4,"Apr",5,"May",6,"Jun",7,"Jul",8,"Aug",9,"Sep",10,"Oct",11,"Nov",12,"Dec") as sales_month
-From (Select s.*,e.region From ${var:database_name}.sales s 
-	join ${var:database_name}.employees e on(s.sales_person_id=e.employee_id)) as se
-	join ${var:database_name}.products p
-	using(product_id);;
+Select se.order_id,se.sales_person_id,se.customer_id,se.product_id,se.product_name,
+se.product_price,se.quantity,se.total_sales_amount, se.order_date,e.region,se.sales_year,se.sales_month
+From ${var:database_name}.product_sales_partition se 
+	join ${var:database_name}.employees e
+	on(se.sales_person_id=e.employee_id);;
 
 invalidate metadata;
-compute stats ${var:database_name}.product_region_sales_partition;
+compute stats ${var:database_name}.product_region_sales_partition; 
